f3e04a386956a316a759c8aac4021b99c0655869
Marek Abaffy <abaffy.m@gmail.com>
2020-08-16 11:16:45 +0200

    Implement Fragment & Activity lifecycle logging (#5)
    * Implement Fragment & Activity lifecycle logging
    
    * Remove logTag from Fragments
    
    Co-authored-by: mabaffy <mabaffy@legalzoom.com>

--- mobile-template-app/src/main/kotlin/cz/helu/templateapp/ui/home/HomeFragment.kt
+++ mobile-template-app/src/main/kotlin/cz/helu/templateapp/ui/home/HomeFragment.kt
@@ -6,6 +6,5 @@ import cz.helu.templateapp.R
 import cz.helu.templateapp.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseViewModelFragment<HomeViewModel, FragmentHomeBinding>(R.layout.fragment_home) {
-    override val logTag: String = HomeFragment::class.java.name
     override val viewModel: HomeViewModel by viewModels()
 }
